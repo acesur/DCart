53b4973f72dc3118e53a71be3e3e0fcaaf473148
Softwarica College Lab <softwarica@softwarica.com>
2020-01-05 14:15:23 +0545

    DBManipulation

--- app/src/main/java/com/example/dcart/CartActivity.java
+++ app/src/main/java/com/example/dcart/CartActivity.java
@@ -1,14 +1,26 @@
 package com.example.dcart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CartActivity extends AppCompatActivity {
 
+
+    private TextView mTextTotal;
+    private Button mButtonContinue, mButtonCheckout;
+    private RecyclerView recyclerView;
+    private DBManipulation mDBManipulation;
+    private SPManipulation mSPManipulation;
+    private ShoppingCartList itemList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+        recyclerView = (RecyclerView) findViewById(R.id.cart_container);
+        mSPManipulation = SPManipulation.getInstance(this);
     }
 }
